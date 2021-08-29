8a86a9bf63881635d2eac9bfb4fd7abc02e556d2
Ian Holmes <concord511@gmail.com>
2021-08-29 16:34:32 -0500

    added functions for prompting user for password criteria

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 // Assignment code here
+var includeLowerCase = function() {
+  var lowerTrue = window.confirm("Password will include at least one LOWER case letter?");
+  if (lowerTrue) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+var includeUpperCase = function() {
+  var upperTrue = window.confirm("Password will include at least one UPPER case letter?");
+  if (upperTrue) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var includeNumber = function() {
+  var numberTrue = window.confirm("Password will include at least one number?");
+  if (numberTrue) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var includeSymbol = function() {
+  var symbolTrue = window.confirm("Password will include at least one symbol?");
+  if (symbolTrue) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var passwordLength = function() {
+  var length = window.prompt("Please enter a numberic value between 8 - 128");
+  length = parseInt(length);
+  if (length >= 8 && length <= 128) {
+    return length;
+  }
+  else {
+    window.alert("Please try again.");
+    passwordLength();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
